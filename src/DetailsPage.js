a209c34a85bb18c8bdbd13f766c8d5220ddb58e8
Jatinkumar Patel <j.patel@se20.qmul.ac.uk>
2022-03-14 23:14:22 +0000

    Added icons for more info

--- src/DetailsPage.js
+++ src/DetailsPage.js
@@ -1,20 +1,47 @@
 import "./detailsPage.css"
+import circle from "./Details/circle_faded.png"
 
 const DetailsPage = () => {
     const current = new Date();
     const date = `${current.getDate()}/${current.getMonth()+1}/${current.getFullYear()}`;
     const time = current.getHours() + ':' + current.getMinutes() + ':' + current.getSeconds();
   return (
-    <div classname = "detailsPage">
+    <div className = "detailsPage">
+        <h2 className = "date">{date}</h2>
+        <h2 className = "time">{time}</h2>
+        <h1 className = "mainTemp"> 8</h1>
+        <p className = "highLowTemps">H: 8 L: 8</p>
+        <p className  = "tempInfo1">morning temp: 10</p>
+        <p className = "tempInfo2">day temp: 10</p>
+        <p className = "tempInfo3">evening temp: 10</p>
+        <p className = "tempInfo4">night temp: 10</p>
+        <table className = "infoTable">
+          <tbody>
+            <tr>
+              <td>
+                <img src = {circle} alt = "info"/>
+              </td>
+              <td>
+                <img src = {circle} alt = "info"/>
+              </td>
+              <td>
+                <img src = {circle} alt = "info"/>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <img src = {circle} alt = "info"/>
+              </td>
+              <td>
+                <img src = {circle} alt = "info"/>
+              </td>
+              <td>
+                <img src = {circle} alt = "info"/>
+              </td>
+            </tr>
+          </tbody>
+        </table>
         
-        <h2 class = "date">{date}</h2>
-        <h2 class = "time">{time}</h2>
-        <h1 class = "mainTemp"> 8</h1>
-        <p class = "highLowTemps">H: 8 L: 8</p>
-        <p class = "tempInfo1">morning temp: 10</p>
-        <p class = "tempInfo2">day temp: 10</p>
-        <p class = "tempInfo3">evening temp: 10</p>
-        <p class = "tempInfo4">night temp: 10</p>
     </div>
   )
 }
